c4e744b4f441984ceac91b0909de6999e771c6e1
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-03-13 03:21:41 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,46 @@
 //         login = true;
 //     }
 // }
+
+//fungsi message
+let username;
+let date;
+let men;
+let women;
+let message;
+
+document.getElementById("submit").onclick = function () {
+  username = document.getElementById("name").value.trim();
+  date = document.getElementById("date").value.trim();
+  men = document.getElementById("men");
+  women = document.getElementById("women");
+  message = document.getElementById("message").value.trim();
+
+  if (
+    username === "" ||
+    date === "" ||
+    message === "" ||
+    (!men.checked && !women.checked)
+  ) {
+    window.alert("Error: Semua input harus diisi!");
+    return; // Hentikan eksekusi lebih lanjut
+  }
+
+  else{
+    if (men.checked) {
+        document.getElementById("output-name").textContent = "Name: " + username;
+        document.getElementById("output-date").textContent = "Date: " + date;
+        document.getElementById("output-gender").textContent = "Gender: Men";
+        document.getElementById("output-message").textContent =
+          "Message: " + message;
+      } else if (women.checked) {
+        document.getElementById("output-name").textContent = "Name: " + username;
+        document.getElementById("output-date").textContent = "Date: " + date;
+        document.getElementById("output-gender").textContent = "Gender: Women";
+        document.getElementById("output-message").textContent =
+          "Message: " + message;
+      } else {
+        window.alert("error");
+      }
+  }
+};
